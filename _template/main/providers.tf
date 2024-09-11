429a2d3c12858f6ed262a8a0189ae2e87efd900b
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2024-09-11 19:21:50 +0200

    chore: fix linting issues in template files

--- _template/main/providers.tf
+++ _template/main/providers.tf
@@ -1,10 +1,17 @@
 terraform {
   required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    }
-    azuread = {
-      source = "hashicorp/azuread"
-    }
+    # Uncomment as needed
+
+    # Lifecycle management of Microsoft Azure using the Azure Resource Manager APIs
+    # https://registry.terraform.io/providers/hashicorp/azurerm/latest
+    # azurerm = {
+    #   source = "hashicorp/azurerm"
+    # }
+
+    # Manage users, groups, service principals, and applications in Azure Active Directory using the Microsoft Graph API.
+    # https://registry.terraform.io/providers/hashicorp/azuread/latest
+    # azuread = {
+    #   source = "hashicorp/azuread"
+    # }
   }
 }
