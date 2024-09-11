0335343a201fb8963d0576277a202266988ab89e
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2024-09-11 19:10:48 +0200

    chore: bump version constraints and state module version

--- _template/envs/_terraform-state/outputs.tf
+++ _template/envs/_terraform-state/outputs.tf
@@ -2,10 +2,12 @@ output "resource_group_name" {
   description = "Name of the resource group created for terraform backend state."
   value       = module.terraform_state_container.resource_group_name
 }
+
 output "storage_account_name" {
   description = "Name of the storage account created for terraform backend state."
   value       = module.terraform_state_container.storage_account_name
 }
+
 output "container_name" {
   description = "Name of the storage container created for terraform backend state."
   value       = module.terraform_state_container.container_name
